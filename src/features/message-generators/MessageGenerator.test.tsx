4f2e906536fb6effe6f937cc3118f1a3e175fea5
HojinAn <devian3143@gmail.com>
2024-05-06 16:13:29 +0900

    feat: 작성한 테스트 코드 통과하도록 연습 1차

--- src/features/message-generators/MessageGenerator.test.tsx
+++ src/features/message-generators/MessageGenerator.test.tsx
@@ -1,11 +1,79 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import { screen } from '@testing-library/react';
+
+import MessageGenerator from '@/features/message-generators/MessageGenerator';
+import render from '@/shared/tests/render';
 
 describe('MessageGenerator', async () => {
-  it('메시지 대상을 선택할 수 있다.', async () => {});
+  it('메시지 대상을 선택할 수 있다.', async () => {
+    const { user } = await render(<MessageGenerator />);
 
-  it('키워드 추가 버튼을 클릭하여 키워드 input을 추가할 수 있다.', async () => {});
+    const select = screen.getByTestId('select-mode');
 
-  describe('키워드 input', async () => {});
+    await user.click(select);
 
-  it('생성 버튼을 클릭하여 메시지를 생성할 수 있다.', async () => {});
+    await user.selectOptions(select, 'family');
+
+    expect(select).toHaveValue('family');
+  });
+
+  it('키워드 추가 버튼을 클릭하여 키워드 input을 추가할 수 있다.', async () => {
+    const { user } = await render(<MessageGenerator />);
+
+    const addButton = screen.getByTestId('add-keyword');
+
+    await user.click(addButton);
+    const inputs1 = screen.getAllByRole('textbox');
+    expect(inputs1).toHaveLength(1);
+
+    await user.click(addButton);
+    const inputs2 = screen.getAllByRole('textbox');
+    expect(inputs2).toHaveLength(2);
+  });
+
+  describe('키워드 input', async () => {
+    it('키워드 input을 통해 키워드를 입력할 수 있다.', async () => {
+      const { user } = await render(<MessageGenerator />);
+
+      const addButton = screen.getByTestId('add-keyword');
+      await user.click(addButton);
+
+      const input = screen.getByTestId('keyword-input-0');
+
+      await user.type(input, '행복');
+      expect(input).toHaveValue('행복');
+    });
+
+    it('키워드 input을 통해 키워드를 수정할 수 있다.', async () => {
+      const { user } = await render(<MessageGenerator />);
+
+      const addButton = screen.getByTestId('add-keyword');
+      await user.click(addButton);
+
+      const input = screen.getByTestId('keyword-input-0');
+
+      await user.type(input, '행복');
+      expect(input).toHaveValue('행복');
+
+      await user.clear(input);
+      await user.type(input, '사랑');
+      expect(input).toHaveValue('사랑');
+    });
+  });
+
+  it('생성 버튼을 클릭하여 선택한 대상과 키워드들을 포함해서 함수를 호출할 수 있다.', async () => {
+    const spy = vi.fn();
+    const { user } = await render(<MessageGenerator onSubmit={spy} />);
+
+    const addButton = screen.getByTestId('add-keyword');
+    await user.click(addButton);
+
+    const input = screen.getByTestId('keyword-input-0');
+    await user.type(input, '행복');
+
+    const generateButton = screen.getByTestId('generate-button');
+    await user.click(generateButton);
+
+    expect(spy).toHaveBeenCalledWith('friend', ['행복']);
+  });
 });
--- src/features/message-generators/MessageGenerator.tsx
+++ src/features/message-generators/MessageGenerator.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useCallback, useState } from 'react';
+
 type GeneratingMode = 'friend' | 'family' | 'work' | 'lover';
 const options: { label: string; value: GeneratingMode }[] = [
   { label: '친구', value: 'friend' },
@@ -7,23 +9,71 @@ const options: { label: string; value: GeneratingMode }[] = [
   { label: '직장', value: 'work' },
   { label: '연인', value: 'lover' },
 ];
-const MessageGenerator = () => {
-  const handleClickToAddKeyword = () => {};
 
-  const handleClickToGenerate = () => {};
+interface Keyword {
+  value: string;
+}
+
+const MessageGenerator = ({
+  onSubmit,
+}: {
+  onSubmit?: (mode: GeneratingMode, keywords: string[]) => void;
+}) => {
+  const [mode, setMode] = useState<GeneratingMode>('friend');
+  const [keywords, setKeywords] = useState<Keyword[]>([]);
+
+  const handleChangeMode = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setMode(event.target.value as GeneratingMode);
+  };
+
+  const handleClickToAddKeyword = () => {
+    setKeywords([...keywords, { value: '' }]);
+  };
+
+  const handleChangeKeyword =
+    (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      const newKeywords = keywords.map((keyword, i) => {
+        if (i === index) {
+          return { ...keyword, value: event.target.value };
+        }
+        return keyword;
+      });
+      setKeywords(newKeywords);
+    };
+
+  const handleClickToGenerate = () => {
+    const keywordValues = keywords.map((keyword) => keyword.value);
+    onSubmit?.(mode, keywordValues);
+  };
 
   return (
-    <>
-      <select>
+    <div>
+      <select onChange={handleChangeMode} data-testid='select-mode'>
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
           </option>
         ))}
       </select>
-      <button onClick={handleClickToAddKeyword}>키워드 추가</button>
-      <button onClick={handleClickToGenerate}>생성</button>
-    </>
+      <button onClick={handleClickToAddKeyword} data-testid='add-keyword'>
+        키워드 추가
+      </button>
+      <ul>
+        {keywords.map((keyword, index) => (
+          <li key={index}>
+            <input
+              type='text'
+              value={keyword.value}
+              onChange={handleChangeKeyword(index)}
+              data-testid={`keyword-input-${index}`}
+            />
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleClickToGenerate} data-testid='generate-button'>
+        생성
+      </button>
+    </div>
   );
 };
 export default MessageGenerator;
