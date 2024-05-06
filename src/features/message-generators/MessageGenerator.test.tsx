6395f1ab80ff4b77190b10381d565c0e6ff128af
HojinAn <devian3143@gmail.com>
2024-05-06 15:16:28 +0900

    feat: test 코드 1차 준비

---
+++ src/features/message-generators/MessageGenerator.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it } from 'vitest';
+
+describe('MessageGenerator', async () => {
+  it('메시지 대상을 선택할 수 있다.', async () => {});
+
+  it('키워드 추가 버튼을 클릭하여 키워드 input을 추가할 수 있다.', async () => {});
+
+  describe('키워드 input', async () => {});
+
+  it('생성 버튼을 클릭하여 메시지를 생성할 수 있다.', async () => {});
+});
