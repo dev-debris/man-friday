3d8f8c025f2b43b18a91abc207c6e2d0d6986ba8
HojinAn <devian3143@gmail.com>
2024-05-06 18:44:44 +0900

    fix: type build에러

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -3,32 +3,28 @@ import OpenAI from 'openai';
 
 const openai = new OpenAI({ apiKey: process.env.OPEN_AI_API_KEY });
 
-export async function POST(req: NextRequest) {
-  try {
-    const body = (await req.json()) as { mode: string; keywords: string[] };
-    const { mode, keywords } = body;
-    const completion = await openai.chat.completions.create({
-      model: 'gpt-4-turbo-2024-04-09',
-      messages: [
-        {
-          role: 'system',
-          content: `#ROLE
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  const body = (await req.json()) as { mode: string; keywords: string[] };
+  const { mode, keywords } = body;
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4-turbo-2024-04-09',
+    messages: [
+      {
+        role: 'system',
+        content: `#ROLE
 You are speaking to a ${mode}. Given keywords, generate something to say in Korean.`,
-        },
-        {
-          role: 'user',
-          content: `# KEYWORDS
+      },
+      {
+        role: 'user',
+        content: `# KEYWORDS
 ${keywords.join(' ')}`,
-        },
-      ],
-    });
+      },
+    ],
+  });
 
-    console.log(completion.choices[0]);
+  console.log(completion.choices[0]);
 
-    return NextResponse.json({
-      message: completion.choices[0],
-    });
-  } catch (error) {
-    return error;
-  }
+  return NextResponse.json({
+    message: completion.choices[0],
+  });
 }
